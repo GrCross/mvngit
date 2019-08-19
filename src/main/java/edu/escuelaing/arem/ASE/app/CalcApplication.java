a562523eee9e362b1048922021c43c1b7dd7cdb6
Daniel <rosalesdaniel350@gmail.com>
2019-08-19 19:02:27 -0400

    detalles

--- src/main/java/edu/escuelaing/arem/ASE/app/CalcApplication.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/CalcApplication.java
@@ -10,19 +10,19 @@ public class CalcApplication {
 
     public static void main(String[] args) throws FileNotFoundException,IOException{
         
-        File file = new File("./folderTest/entry.in"); 
+        File file = new File("./inputFolder/entry.in"); 
         Calc calc = new Calc(file);
 
-        FileWriter fw = new FileWriter("./folderTest/answer.out");    
+        FileWriter fw = new FileWriter("./inputFolder/answer.out");    
         fw.write("mean:\n");
         fw.write(calc.mean()+"\n");
         fw.write("desviation: \n");
         fw.write(calc.desviation()+"\n");
         fw.close();
 
-        file = new File("./folderTest/entry2.in"); 
+        file = new File("./inputFolder/entry2.in"); 
         calc = new Calc(file);
-        fw = new FileWriter("./folderTest/answer2.out");    
+        fw = new FileWriter("./inputFolder/answer2.out");    
         fw.write("mean:\n");
         fw.write(calc.mean()+"\n");
         fw.write("desviation: \n");
